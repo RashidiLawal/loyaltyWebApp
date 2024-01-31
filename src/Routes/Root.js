d5f011d86701a9a13f4a6826b7c4f8128ef4ee31
Lawal Rashidi <lawalrashidi95@gmail.com>
2024-01-31 22:10:49 +0100

    folder setup

---
+++ src/Routes/Root.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SideBar } from '../components/SideBar/SideBar';
+
+ const Root = () => {
+  return (
+    <SideBar/>
+  )
+}
+
+export default Root;
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -2,6 +2,32 @@ import React from 'react'
 
 export const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <aside>
+        <h2>
+            company Logo
+        </h2>
+        <nav>
+            <ul>
+            <li>
+            <a href="">Overview</a>
+            </li>
+            <li>
+            <a href="">Products</a>
+            </li>
+            <li>
+            <a href="">Order</a>
+            </li>
+            <li>
+            <a href="">Customers</a>
+            </li>
+            <li>
+            <a href="">Promotion</a>
+            </li>
+            <li>
+            <a href="">Settings</a>
+            </li>
+            </ul>
+        </nav>
+    </aside>
   )
 }
